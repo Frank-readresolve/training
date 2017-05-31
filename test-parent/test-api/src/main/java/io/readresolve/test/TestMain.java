aad7859446fbaa9a1697a2898883e2ef7583454f
Frank MARSHALL <frank@readresolve.io>
2017-05-31 10:27:40 +0200

    Formating and Javadoc added

--- test-parent/test-api/src/main/java/io/readresolve/test/TestMain.java
+++ test-parent/test-api/src/main/java/io/readresolve/test/TestMain.java
@@ -1,10 +1,17 @@
 package io.readresolve.test;
-
+/**
+ * 
+ * @author Frank
+ *
+ */
 public class TestMain {
 
+	/**
+	 * 
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Git!");
+		System.out.println("Hello Git!");
 	}
 
 }
