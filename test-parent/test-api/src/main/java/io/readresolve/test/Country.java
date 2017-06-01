8b943b9714f9c296e017260f9ff2d4341f3558ef
Frank MARSHALL <frank@readresolve.io>
2017-06-01 15:10:25 +0200

    New Country class

---
+++ test-parent/test-api/src/main/java/io/readresolve/test/Country.java
@@ -0,0 +1,35 @@
+package io.readresolve.test;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public final class Country implements Serializable {
+
+	private static final long serialVersionUID = 8240881715822890568L;
+	
+	private final String code;
+
+	public Country(String code) {
+		Objects.requireNonNull(code);
+		this.code = code;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Country))
+			return false;
+		Country other = (Country) obj;
+		return code.equals(other.code);
+	}
+
+	@Override
+	public String toString() {
+		return code;
+	}
+}
