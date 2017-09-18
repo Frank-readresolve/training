7494d8d7fe9aa65f92a92b9c40feaddb6e8eb6b4
Frank MARSHALL <frank@readresolve.io>
2017-09-18 22:34:19 +0200

    Changes

--- test-parent/test-api/src/main/java/io/readresolve/test/MathUtil.java
+++ test-parent/test-api/src/main/java/io/readresolve/test/MathUtil.java
@@ -1,13 +1,16 @@
 package io.readresolve.test;
 
 public class MathUtil {
-	
+
 	public static int sum(int x, int y) {
-		return x + y;
+		if(x>0) {
+			return x + y;	
+		}
+		if(x>0) {
+			return x + y;	
+		}
+		else
+			return x + y;	
 	}
-	
-	public static int sum2(int x, int y) {
-		return x + y;
-	}
-	
+
 }
