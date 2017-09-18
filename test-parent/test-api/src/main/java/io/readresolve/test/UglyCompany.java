8b59c124a6eeba30e813266e856d4c0399cc4581
Frank MARSHALL <frank@readresolve.io>
2017-09-18 13:55:36 +0200

    Some change

--- test-parent/test-api/src/main/java/io/readresolve/test/UglyCompany.java
+++ test-parent/test-api/src/main/java/io/readresolve/test/UglyCompany.java
@@ -1,25 +1,29 @@
 package io.readresolve.test;
 
 public class UglyCompany {
-	
+
 	public String name = null;
-	
+
 	private Country country;
-	
+
 	public UglyCompany() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	
-	 UglyCompany(String name) {
+
+	UglyCompany(String name) {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public String getName() {
 		System.out.println(name);
 		return name;
 	}
-	
+
+	/**
+	 * Returns the name of the ugly company.
+	 * 
+	 * @return the name
+	 */
 	@Override
 	public String toString() {
 		return null;
