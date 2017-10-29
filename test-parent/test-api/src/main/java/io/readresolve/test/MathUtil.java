a21fdc1653f16b29b6daa3a4ea3cbe66edb3e204
Frank MARSHALL <frank@readresolve.io>
2017-10-29 19:00:33 +0100

    New methods

--- test-parent/test-api/src/main/java/io/readresolve/test/MathUtil.java
+++ test-parent/test-api/src/main/java/io/readresolve/test/MathUtil.java
@@ -2,7 +2,17 @@ package io.readresolve.test;
 
 public class MathUtil {
 
+    public static int VAL = 5;
+
     public static int sum(int x, int y) {
 	return x + y;
+    }
+
+    public static int max(int x, int y) {
+	throw new UnsupportedOperationException("not implemented");
+    }
+
+    public static boolean isGreater(int other) {
+	return other > VAL;
     }
 }
