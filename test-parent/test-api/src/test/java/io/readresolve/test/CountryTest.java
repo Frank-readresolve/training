ec0610acb0a6b450e36e839c3b7d4792549ffe06
Lamine <Lamine@LamPC2>
2017-10-31 14:51:05 +0100

    Tests unitaires réalisés

--- test-parent/test-api/src/test/java/io/readresolve/test/CountryTest.java
+++ test-parent/test-api/src/test/java/io/readresolve/test/CountryTest.java
@@ -17,4 +17,39 @@ public class CountryTest {
 		Country country = new Country("");
 		assertNotNull(country);
 	}
+        
+        @Test()
+        public void testCalculmethode() {
+                Country country = new Country("");
+                country.Calculmethode();
+                assertNotNull(country);
+        }
+        
+        @Test()
+        public void testGetCode() {
+                Country country = new Country("");
+                country.getCode();
+                assertNotNull(country);
+        }
+        
+        @Test()
+        public void testEquals() {
+                Country country = new Country("");
+                Object obj = new Object();
+                assertNotNull(country.equals(obj));
+        }
+        
+        @Test(expected = NullPointerException.class)
+        public void testHashCode() {
+                Country country = new Country("");
+                country.hashCode();
+                assertNotNull(country);
+        }
+        
+        @Test(expected = AssertionError.class)
+        public void testToString() {
+                Country country = new Country("");
+                country.toString();
+                assertNull(country);
+        }
 }
