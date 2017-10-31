682e2ffacc6ba600bd9f6c157bde0aad4cd875bd
Jonathan Louisor <j_louisor@orange.fr>
2017-10-31 14:53:49 +0100

    Company mieux couvert

--- test-parent/test-api/src/test/java/io/readresolve/test/CompanyTest.java
+++ test-parent/test-api/src/test/java/io/readresolve/test/CompanyTest.java
@@ -5,27 +5,50 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CompanyTest {
-	@Test()
-	public void shouldConstruct() {
-		Company company = new Company("");
-		assertNotNull(company);
-	}
-	
-	@Test()
-	public void shouldConstructAslo() {
-		Company company = new Company();
-		assertNotNull(company);
-	}
-	
-	@Test(expected=NullPointerException.class)
-	public void shouldFail() {
-		Company company = new Company(null);
-		assertNotNull(company);
-	}
 
-	@Test()
-	public void shouldReturnName() {
-		Company company = new Company("NAME");
-		assertNotNull(company.toString());
-	}
+    @Test()
+    public void shouldConstruct() {
+        Company company = new Company("");
+        assertNotNull(company);
+    }
+
+    @Test()
+    public void shouldConstructAslo() {
+        Company company = new Company();
+        assertNotNull(company);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldFail() {
+        Company company = new Company(null);
+        assertNotNull(company);
+    }
+
+    @Test()
+    public void shouldReturnName() {
+        Company company = new Company("NAME");
+        assertNotNull(company.toString());
+    }
+
+    @Test()
+    public void shouldSetName() {
+        Company company = new Company("SANS NOM");
+        company.setName("Ivalice");
+        assertEquals("Ivalice", company.getName());
+    }
+
+    @Test()
+    public void shouldEquals() {
+        Company company = new Company("YES");
+        Company company2 = new Company("YES");
+        assertEquals(company2, company);
+    }
+
+    @Test()
+    public void shouldHashCode() {
+        Company company = new Company("YES");
+        Company company2 = new Company("YES");
+        assertEquals(company.hashCode(), company2.hashCode());
+    }
+
 }
